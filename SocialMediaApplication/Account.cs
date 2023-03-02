b7e7466c4e98fe06a1d0d64ce7d22a3fd5f7db6d
Harley Gotardo <106173250+HarleyGotardo@users.noreply.github.com>
2023-03-02 23:15:04 +0800

    Add project files.

---
+++ SocialMediaApplication/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaApplication
+{
+    public abstract class Account
+    {
+        public string Username { get; set; }
+        public string Password { private get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        
+        public Account(string username, string password, string email, string phoneNumber)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Email = email;
+            this.PhoneNumber = phoneNumber;
+        }
+    }
+}
---
+++ SocialMediaApplication/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaApplication
+{
+    public abstract class User : Account
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public DateOnly BirthDate { get; private set; }
+        public string Status { get; set; }
+        public string Bio { get; set; }
+        public string Address { get; set; }
+
+        public User(string username, string password, string email, string phoneNumber, string firstName, string lastName, int age, DateOnly birthDate, string status) : base(username, password, email, phoneNumber)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.BirthDate = birthDate;
+            this.Status = status;
+        }
+    }
+}
